b34cb895a7fa693d180b4d32b07661a73a39ec7a
José López <jose.lopez@acklenavenue.com>
2025-10-30 22:39:22 -0600

    refactor: remove Google and Microsoft login functionalities from authentication module

--- client/src/firebase/auth/index.ts
+++ client/src/firebase/auth/index.ts
@@ -1,4 +1,2 @@
 export { loginWithEmail } from "./loginWithEmail";
 export { sendPasswordResetEmail } from "./sendPasswordResetEmail";
-export { loginWithGoogle } from "./loginWithGoogle";
-export { loginWithMicrosoft } from "./loginWithMicrosoft";
